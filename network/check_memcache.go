b1e579e3eaafbfd9669593d2b0d0ed0d05eac2d2
Chris Mague <github@mague.com>
2013-08-30 15:51:07 -0700

    added check_memcache script

---
+++ network/check_memcache.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"flag"
+	"github.com/bradfitz/gomemcache/memcache"
+	"fmt"
+	"os"
+	"time"
+)
+
+var hostname string
+var port int
+var timeout int
+
+func init() {
+	flag.StringVar(&hostname, "hostname", "localhost", "hostname or ip of memcache server")
+	flag.IntVar(&port, "port", 11211, "port to try to connect to")
+	flag.IntVar(&timeout, "timeout", 3, "number of seconds to wait for connection")
+	flag.Parse()
+}
+
+func main() {
+
+	c1 := make(chan bool)
+
+	//##################################################################################
+	go func() {
+		for {
+			mc := memcache.New(fmt.Sprintf("%s:%d", hostname, port))
+			mc.Set(&memcache.Item{Key: "dtm-not-real", Value: []byte("setByNagios")})
+			conn, err := mc.Get("dtm-not-real")
+			if err != nil {
+				fmt.Println(fmt.Sprintf("CRITICAL: %s", err))
+				c1 <- false
+			}
+			if conn != nil {
+				c1 <- true
+			}
+
+			mc.Delete("dtm-not-real")
+
+		}
+	}()
+	//##################################################################################
+	go func() {
+		time.Sleep(time.Duration(timeout) * time.Second)
+		c1 <- false
+	}()
+	//##################################################################################
+	go func () {
+		for {
+			select {
+				case msg1 := <- c1:
+					if msg1 {
+							fmt.Println("OK: able to create and fetch a record")
+						os.Exit(0)
+					} else {
+						os.Exit(2)
+					}
+				}
+		}
+	}()
+	var input bool
+	fmt.Scanln(&input)
+}
