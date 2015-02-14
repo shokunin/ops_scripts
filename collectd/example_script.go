92e28c74d25533851dcb6c3cdcc4e818ab996a9f
Chris Mague <github@mague.com>
2015-02-14 10:59:33 -0800

    add in the host information quickly

--- collectd/example_script.go
+++ collectd/example_script.go
@@ -9,31 +9,40 @@ import "strconv"
 
 var sleepTime int
 var debug bool
+var host string
 
-func collectd(unixTs int) {
+func collectd(unixTs int, hostname string) {
+	var hostlabel string
+	if hostname == "localhost" {
+		hostlabel, _ = os.Hostname()
+	} else {
+		hostlabel = hostname
+	}
+
 	f := bufio.NewWriter(os.Stdout)
 	defer f.Flush()
-	b := "PUTVAL " + "foo" + "/" + "bar" + "/" + "gauge-name " + strconv.Itoa(unixTs) + ":" + "value\n"
+	b := "PUTVAL " + hostlabel + "/" + "bar" + "/" + "gauge-name " + strconv.Itoa(unixTs) + ":" + "value\n"
 	f.Write([]byte(b))
 }
 
 func init() {
 	flag.BoolVar(&debug, "debug", false, "turn on debugging")
 	flag.IntVar(&sleepTime, "sleep-time", 10, "Number of seconds between runs")
+	flag.StringVar(&host, "host to connect to", "localhost", "Defaults to localhost")
 	flag.Parse()
 }
 
 func main() {
 	ticker := time.NewTicker(time.Second * time.Duration(sleepTime))
 	// run once at the beginning
-	collectd(int(time.Now().Unix()))
+	collectd(int(time.Now().Unix()), host)
 	go func() {
 		//for t := range ticker.C {
 		for t := range ticker.C {
 			if debug {
 				fmt.Println("Tick at", time.Now(), " DOH ", t)
 			} else {
-				collectd(int(time.Now().Unix()))
+				collectd(int(time.Now().Unix()), host)
 			}
 		}
 	}()
