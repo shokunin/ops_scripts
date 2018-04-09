659c608144110271b0a5bd5284c2b1cb9282c453
Christian Mague <cmague@konghq.com>
2018-04-09 13:44:24 -0700

    an example script to get some inforamtion when there is no object debug command

---
+++ network/redis_keys_size_no_debug.go
@@ -0,0 +1,102 @@
+package main
+
+// MAKE SURE TO SET export GOMAXPROCS=16  in your environment before running
+
+import "fmt"
+import "strings"
+import "flag"
+import "time"
+import "os"
+import "unsafe"
+import "github.com/fzzy/radix/redis"
+
+func fetchAllKeys(hostame string, port int, database int) []string {
+	c, err := redis.DialTimeout("tcp", fmt.Sprintf("%s:%d", hostname, port), time.Duration(300)*time.Second)
+	errHndlr(err)
+	keys := c.Cmd("SELECT", database)
+	keys = c.Cmd("KEYS", "*")
+	j := keys.Elems
+	redis_keys := make([]string, len(j), len(j))
+	for i := 0; i < len(j); i++ {
+		redis_keys[i] = fmt.Sprintf("%s", j[i])
+	}
+	return redis_keys
+}
+
+func errHndlr(err error) {
+	if err != nil {
+		fmt.Println("error:", err)
+		os.Exit(1)
+	}
+}
+
+func worker(id int, jobs <-chan string, results chan<- string, hostame string, port int, database int) {
+	c, err := redis.DialTimeout("tcp", fmt.Sprintf("%s:%d", hostname, port), time.Duration(3)*time.Second)
+	errHndlr(err)
+	for j := range jobs {
+		rsize := uint64(0)
+		errHndlr(err)
+		c.Cmd("SELECT", database)
+		z := c.Cmd("TYPE", j)
+		w, _ := z.Bytes()
+		if string(w) == "string" {
+			k := c.Cmd("GET", j)
+			rsize += uint64(unsafe.Sizeof(k))
+			y, _ := k.Bytes()
+			rsize += uint64(len(y))
+			fmt.Println("total size: ", rsize, "customer: ", j)
+		}
+		if string(w) == "hash" {
+			k := c.Cmd("HGETALL", j)
+			s := strings.Split(j, ":")
+			rsize += uint64(unsafe.Sizeof(k))
+			if k.Type > 4 {
+				for _, x := range k.Elems {
+					y, _ := x.Bytes()
+					rsize += uint64(len(y))
+				}
+			}
+			fmt.Println("total size: ", rsize, "customer: ", s[len(s)-1])
+		}
+		results <- "OK"
+	}
+	c.Close()
+}
+
+var hostname string
+var port int
+var concurrent int
+var database int
+
+func init() {
+	flag.StringVar(&hostname, "hostname", "localhost", "hostname or ip to scan")
+	flag.IntVar(&port, "port", 6379, "port to try to connect to")
+	flag.IntVar(&concurrent, "concurrent", 10, "number of workers to run")
+	flag.IntVar(&database, "database", 0, "Redis database to use.  DB 0 is the default")
+	flag.Parse()
+}
+
+func main() {
+
+	keys := fetchAllKeys(hostname, port, database)
+	// In order to use our pool of workers we need to send
+	// them work and collect their results. We make 2
+	// channels for this.
+	jobs := make(chan string, len(keys))
+	results := make(chan string, len(keys))
+
+	for w := 0; w <= concurrent; w++ {
+		go worker(w, jobs, results, hostname, port, database)
+	}
+
+	for j := 0; j <= len(keys)-1; j++ {
+		jobs <- keys[j]
+	}
+	close(jobs)
+
+	// Finally we collect all the results of the work.
+	for a := 0; a <= len(keys)-1; a++ {
+		<-results
+	}
+	os.Exit(0)
+}
